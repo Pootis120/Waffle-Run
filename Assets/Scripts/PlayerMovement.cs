94530b78e921a1036856f50a013d1166aa5c9f73
unknown <danya.mokhov@gmail.com>
2022-02-14 22:21:11 +0300

    Basic project setup

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector3 Position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
