c9ff4c96359ec335b742dfa0731f96f777afa7da
unknown <danya.mokhov@gmail.com>
2022-02-15 22:55:24 +0300

    The top and bottom bounds are finished

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public Vector3 Position;
     Rigidbody Rb;
     public float m_Thrust = 20f;
+    public int health = 150;
+    bool onGround = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +24,29 @@ public class PlayerMovement : MonoBehaviour
             //Apply a force to this Rigidbody in direction of this GameObjects up axis
             Rb.AddForce(transform.up * m_Thrust);
         }
+        if (onGround) 
+        {
+            health -= 1;
+        }
     }
 
+    //More reliable for some reason than OnCollisionStay
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.transform.tag) 
+        {
+            case "Ground" :
+                onGround = true;
+                break;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        switch (collision.transform.tag)
+        {
+            case "Ground":
+                onGround = false;
+                break;
+        }
+    }
 }
