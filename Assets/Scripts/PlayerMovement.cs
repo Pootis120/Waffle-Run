4b3e11a9a8182cd0ec2b37b0847774039786127f
unknown <danya.mokhov@gmail.com>
2022-02-15 18:37:15 +0300

    Added movement and changed camera to ortho

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Vector3 Position;
+    Rigidbody Rb;
+    public float m_Thrust = 20f;
     // Start is called before the first frame update
     void Start()
     {
         Position = transform.position;
+        Rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetButton("Jump"))
+        {
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            Rb.AddForce(transform.up * m_Thrust);
+        }
     }
 }
