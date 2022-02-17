55681d6f0f5682fcfcd272d6e91974a9f16933c6
andrey-aka-skif <2119163@gmail.com>
2022-02-17 23:20:06 +0300

    Альтернативные реализации

---
+++ Assets/Scripts/PlayerMovementAlt.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovementAlt : MonoBehaviour
+{
+    [SerializeField] private float _thrust = 20f;
+    [SerializeField] private int _health = 150;
+
+    private const string _groundTag = "Ground";
+
+    private Rigidbody _rb;
+    private bool _onGround = false;
+    
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButton("Jump"))
+            _rb.AddForce(Vector3.up * _thrust);
+
+        if (_onGround)
+            _health--;
+    }
+
+    //More reliable for some reason than OnCollisionStay
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Ground")
+            _onGround = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+            _onGround = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMovementWakeUped.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovementWakeUped : MonoBehaviour
+{
+    [SerializeField] private float _thrust = 20f;
+    [SerializeField] private int _health = 150;
+
+    private const string _groundTag = "Ground";
+
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButton("Jump"))
+            _rb.AddForce(Vector3.up * _thrust);
+
+        if (_rb.IsSleeping())
+            _rb.WakeUp();
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+            _health--;
+    }
+}
